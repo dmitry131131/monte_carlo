008425205804f7cdacef4f9f512bcdcf92d097f4
Dmitry Antontsev <antontsev.dmitry@yandex.ru>
2025-09-03 18:21:27 +0300

    [Monitor] Decide to manage temperature manually with /thermal_zone
    + Also should add instance for windows and other os

--- include/Core/Core.hpp
+++ include/Core/Core.hpp
@@ -5,7 +5,8 @@
 #include "libcpuid.h"
 
 
-/// This class implements machine information data
+/// This class implements machine information data and system monitor
+/// This class collecting information about CPU temperature during algorithm
 class Machine {
     using temp_t = double; 
     using time_point_t = std::chrono::time_point<std::chrono::steady_clock>;
@@ -13,6 +14,8 @@ class Machine {
 
     int core_count_;    /// count of available logical cpus
     bool valid_;        /// valid status of machine
+    // TODO PID
+    // TODO OS_TYPE_ENUM -> to use consistent temperature getter
 
     temp_history_t temperature_;
 public:
@@ -22,9 +25,14 @@ public:
     unsigned get_core_count() const {return core_count_;}
     temp_history_t &get_temperature_history() {return temperature_;}
 
+    // TODO template get_temperature function. Set instance for Linux and Windows 
+
     // TODO write temperature monitoring functions
     // Machine should start monitor process control it by IPC communication 
+
+    // First method
     // Also should wait process ending after benchmark and copy temp vector to machine
+    // In this function need to create shared memory to copy temperature vector into Machine
 };
 
 // Function dumps cpuid data in pretty view
